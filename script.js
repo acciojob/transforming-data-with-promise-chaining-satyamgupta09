baee0d4294b2d15f1d98ed853c4be1ebdaf02e08
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-27 22:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ document.getElementById("btn").addEventListener('click', function() {
 function processNumber(num){
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
-			document.getElementById("output").innerHTML=num;
+			document.getElementById("output").innerHTML+=num;
 		    resolve(num);
 		},2000)
 	}).then(function(num){
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
 			let newNum = num*2;
-			// document.getElementById("output").innerHTML='';
-			document.getElementById("output").innerHTML=newNum;
+			// document.getElementById("output").innerHTML+='';
+			document.getElementById("output").innerHTML+=newNum;
 		    resolve(newNum);
 		},2000)
 	});
@@ -31,8 +31,8 @@ function processNumber(num){
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
 			let newNum = num - 3;
-			// document.getElementById("output").innerHTML='';
-			document.getElementById("output").innerHTML=newNum;
+			// document.getElementById("output").innerHTML+='';
+			document.getElementById("output").innerHTML+=newNum;
 		    resolve(newNum);
 		},1000)
 	});
@@ -40,8 +40,8 @@ function processNumber(num){
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
 			let newNum = num / 2;
-			// document.getElementById("output").innerHTML='';
-			document.getElementById("output").innerHTML=newNum;
+			// document.getElementById("output").innerHTML+='';
+			document.getElementById("output").innerHTML+=newNum;
 		    resolve(newNum);
 		},1000)
 	});
@@ -49,8 +49,8 @@ function processNumber(num){
 	return new Promise((resolve)=>{
 		setTimeout(()=>{
 			let newNum = num + 10;
-			// document.getElementById("output").innerHTML='';
-			document.getElementById("output").innerHTML=newNum;
+			// document.getElementById("output").innerHTML+='';
+			document.getElementById("output").innerHTML+=newNum;
 		    resolve(newNum);
 		},1000)
 	});
